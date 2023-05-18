f28927138890bcd654c79472945bab18c3ca1924
dewikhulbi <dewikhlb20@gmail.com>
2023-05-18 15:19:38 +0700

    untuk ditampilkan di layar

--- 132_singlelinkedlist/132_singlelinkedlist.cpp
+++ 132_singlelinkedlist/132_singlelinkedlist.cpp
@@ -123,3 +123,22 @@ void searchData() {
 		cout << "Data tidak detemukan" << endl;
 	}
 }
+
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+
+
+
+		}
+	}
+}
